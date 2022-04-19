6eecd9c07e5c5057f3ce78ad5873ccfad2c85196
OGURCHIK <103943346+OGURCHIK248@users.noreply.github.com>
2022-04-19 19:44:31 +0700

    Добавил функцию
    скопировал функцию natasha в мультиплеер
    +подогнал по РЕР8

--- Sing/Natasha.py
+++ Sing/Natasha.py
@@ -1,7 +1,7 @@
 def natasha(text):
-	text=text.replace(",","")
-	text=text.replace(".","")
-	text=text.replace("!","")
-	text=text.replace("?","")
-	text=text.split()
-	return text
+    text = text.replace(",", "")
+    text = text.replace(".", "")
+    text = text.replace("!", "")
+    text = text.replace("?", "")
+    text = text.split()
+    return text
